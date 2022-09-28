c833da3d640b292e214f3b251577f457bc44e152
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-09-28 18:06:02 +0200

    navbar list item data

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="#">
+            <img
+              src="https://news.ycombinator.com/y18.gif"
+              width="40"
+              height="40"
+            />
+          </a>
+
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">Home</a>
+
+            <a className="navbar-item">Documentation</a>
+
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">More</a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item">About</a>
+                <a className="navbar-item">Jobs</a>
+                <a className="navbar-item">Contact</a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">Report an issue</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field has-addons">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Find a repository"
+                  />
+                </div>
+                <div className="control">
+                  <a className="button is-info">Search</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
