b38eeba0a6a91cfcada2a47ff283fe79ec472bb3
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-09-29 21:38:45 +0200

    pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Pagination({ page, setPage, rangeLength }) {
+
+    let pages = [];
+    const offset = page < 5 ? 5 - page : 0;
+    for (let i = page - 5; i <= page + 5; i++) {
+    pages.push(i + offset);
+  }
+  return pages.map((pn) => 
+    <>
+        <button className="pagination-button" onClick={() => setPage(pn)}>{pn}</button>
+            
+    </>
+    );
+}
+    
